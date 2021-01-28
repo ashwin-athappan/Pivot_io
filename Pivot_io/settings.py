4b9d98b03d39e216d4a3aa007d7c357c8942c043
AshwinAthappan <32098750+ashwin-athappan@users.noreply.github.com>
2021-01-28 11:45:47 +0530

    sqlite3 version

--- Pivot_io/settings.py
+++ Pivot_io/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'Pivot_io.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pivot_io',
-        'USER': 'postgres',
-        'PASSWORD': '1379',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
